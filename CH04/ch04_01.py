91e1b0e37c2c40cca06d33f0ab7286e2ce704fad
Yen31 <82165728+yenyen31@users.noreply.github.com>
2023-05-18 09:42:15 +0900

    Create ch04_01.py

---
+++ CH04/ch04_01.py
@@ -0,0 +1,51 @@
+# 101에서 110까지 구하는 코드 - 회귀 모델 사용하기
+from keras.models import Sequential
+from keras.layers import Dense
+import numpy as np
+
+# (1) 데이터 준비
+x_train = np.array([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
+y_train = np.array([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
+x_test = np.array([101, 102, 103, 104, 105, 106, 107, 108, 109, 110])
+y_test = np.array([101, 102, 103, 104, 105, 106, 107, 108, 109, 110])
+
+# (2) 모델 구성
+model = Sequential()
+model.add(Dense(5, input_dim=1, activation="relu"))  # 1개의 입력으로 5개의 노드 출력하기
+model.add(Dense(3))  # 5개의 입력을 다시 3개의 노드로 출력하기
+model.add(Dense(1, activation="relu"))  # 3개의 입력을 받아 1개로 출력하기
+
+# model.summary()  # 모델 구성 확인하기
+
+# (3) 컴파일, 훈련
+model.compile(
+    loss="mse", optimizer="adam", metrics=["mse"]
+)  # 기존 코드에서 mse로 변경 -> 머신이 훈련할 때 보여주는 부분이 acc대신 mse로 표현하겠다는 뜻
+
+# validation_data에 원래 훈련과는 다른 평가용 데이터 입력하기
+model.fit(x_train, y_train, epochs=100, batch_size=1, validation_data=(x_test, y_test))
+loss, acc = model.evaluate(x_test, y_test, batch_size=1)
+
+# (4) 평가, 예측
+print("loss : ", loss)
+print("acc : ", acc)
+
+# 다른 데이터의 예측값 사용하기
+output = model.predict(x_test)
+print("결과물 : \n", output)
+
+# RMSE(평균 제곱근 오차) 구하기
+from sklearn.metrics import mean_squared_error
+
+
+def RMSE(y_test, y_predict):
+    return np.sqrt(mean_squared_error(y_test, y_predict))
+
+
+print("RMSE : ", RMSE(y_test, y_predict))
+
+# R2(결정계수) 구하기 -> 사이킷 런에서 함수형태로 제공해줘서 import해서 그대로 사용하면 됨
+from sklearn.metrics import r2_score
+
+r2_y_predict = r2_score(y_test, y_predict)
+print("R2 : ", r2_y_predict)
